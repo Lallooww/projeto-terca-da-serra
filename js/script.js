6124330cf1a152da6635f40a9dc2e6f3f414b47d
Lallooww <laradawson288@gmail.com>
2024-05-24 16:38:18 -0300

    imagem de exemplo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-
-//função para exibir a data e a hora em tempo real
+//begin index
+// Função para exibir a data e a hora em tempo real
 function updateDateTime() {
     const now = new Date();
     const date = now.toLocaleDateString();
     const time = now.toLocaleTimeString();
-    document.getElementById('date-time').innerText = `${date} - ${time}`;
+    const dateTimeElement = document.getElementById('date-time');
+    if (dateTimeElement) {
+        dateTimeElement.innerText = `${date} - ${time}`;
+    }
 }
 
 // Atualiza a data e hora assim que a página é carregada
@@ -12,10 +15,16 @@ window.onload = function() {
     updateDateTime();
     // Atualiza a data e hora a cada segundo
     setInterval(updateDateTime, 1000);
-};
 
-//direciona para outra página após o login
-document.getElementById("login-button").addEventListener("click", function() {
-    // Redireciona para a página desejada após o login
-    window.location.href = "home-page.html";
-});
+    // Verifica se o botão de login existe antes de adicionar o ouvinte de eventos
+    const loginButton = document.getElementById("login-button");
+    if (loginButton) {
+        loginButton.addEventListener("click", function() {
+            // Redireciona para a página desejada após o login
+            window.location.href = "home-page.html";
+        });
+    }
+}
+//end index
+
+
